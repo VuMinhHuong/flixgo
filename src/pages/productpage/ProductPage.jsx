2071801dd12658ab70c7a313a55dfea7f1034fcf
HuongVM <vuminhhuong.vn@gmail.com>
2023-02-24 17:34:13 +0700

    2402-v2

--- src/pages/productpage/ProductPage.jsx
+++ src/pages/productpage/ProductPage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/homePage/Navbar'
 import footer from "../../assets/img/footer.JPG"
 import Product from "../../components/product/Product"
 import Comment from "../../components/product/Comment"
+import NewItemExpect from "../../components/homePage/NewItemExpect"
 
 
 const Hompage = () => {
@@ -31,28 +32,45 @@ const Hompage = () => {
         </div>
 
         <div className="productpage-discover">
-              <div className="menu-item">
-                <div className="menu-item-top">Discover</div>
-                <div className="menu-item-down">
-                  <span>COMMENTS</span>
-                  <span>REVIEWS</span>
-                  <span>PHOTOS</span>
-                </div>
+          <div className="menu-item">
+            <div className="menu-item-top">Discover</div>
+            <div className="menu-item-down">
+              <span>COMMENTS</span>
+              <span>REVIEWS</span>
+              <span>PHOTOS</span>
+            </div>
+          </div>
+          <div className="productpage-container">
+            <div className="item-container">
+              <Comment/>
+              <Comment/>
+              <Comment/>
+              <Comment/>
+              <Comment/>
+            </div>
+            <div className="also-like">
+              <div className="also-like-item">
+                <NewItemExpect/>
               </div>
-              <div className="productpage-container">
-                <div className="item-container">
-                  <Comment/>
-                  <Comment/>
-                  <Comment/>
-                  <Comment/>
-                  <Comment/>
-                </div>
-                <div className="also-like">
-
-                </div>
-  
+              <div className="also-like-item">
+                <NewItemExpect/>
+              </div>
+              <div className="also-like-item">
+                <NewItemExpect/>
+              </div>
+              <div className="also-like-item">
+                <NewItemExpect/>
+              </div>
+              <div className="also-like-item">
+                <NewItemExpect/>
+              </div>
+              <div className="also-like-item">
+                <NewItemExpect/>
               </div>
             </div>
+
+          </div>
+        </div>
 
         <div className="footer-img">
           <img src={footer} alt="footer-img" />
